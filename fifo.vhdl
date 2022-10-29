c344d587e5c147ababd307e8ea057cbf6ccb7bf8
Carbon225 <carbon225@protonmail.com>
2022-10-29 16:13:15 +0200

    fifo

--- fifo.vhdl
+++ fifo.vhdl
@@ -4,16 +4,23 @@ use ieee.std_logic_1164.all;
 entity fifo is
     generic (
         c_width : positive;
-        c_depth : positive
+        c_depth : positive;
+        c_thr : natural
     );
     port (
         i_clk : in std_logic;
-        i_write : in std_logic;
-        i_read : in std_logic;
-        o_empty : out std_logic;
-        o_full : out std_logic;
+        
+        i_valid : in std_logic;
+        o_ready : out std_logic;
         i_data : in std_logic_vector(c_width - 1 downto 0);
-        o_data : out std_logic_vector(c_width - 1 downto 0)
+
+        o_valid : out std_logic;
+        i_ready : in std_logic;
+        o_data : out std_logic_vector(c_width - 1 downto 0);
+
+        o_empty : out std_logic;
+        o_thr : out std_logic;
+        o_full : out std_logic
     );
 end entity fifo;
 
@@ -21,29 +28,54 @@ architecture rtl of fifo is
 
     type t_mem is array(c_depth - 1 downto 0) of std_logic_vector(c_width - 1 downto 0);
     signal r_mem : t_mem := (others => (others => '0'));
-    signal r_out : std_logic_vector(c_width - 1 downto 0) := (others => '0');
-    
+
+    signal r_size : integer range 0 to c_depth := 0;
+
+    signal r_ready : std_logic;
+    signal r_valid : std_logic;
+
+    signal r_read : std_logic;
+    signal r_write : std_logic;
+
+    signal r_empty : std_logic;
+    signal r_thr : std_logic;
+    signal r_full : std_logic;
+
     signal r_rd_head : integer range 0 to c_depth - 1 := 0;
     signal r_wr_head : integer range 0 to c_depth - 1 := 0;
 
 begin
-    
-    o_empty <= '1' when r_rd_head = r_wr_head else '0';
-    o_full <= '1' when r_rd_head = r_wr_head + 1 else '0';
 
-    o_data <= r_out;
+    r_empty <= '1' when r_size = 0 else '0';
+    r_thr <= '1' when r_size >= c_thr else '0';
+    r_full <= '1' when r_size = c_depth else '0';
+
+    r_valid <= not r_empty;
+    r_ready <= not r_full;
+
+    r_read <= '1' when r_valid = '1' and i_ready = '1' else '0';
+    r_write <= '1' when i_valid = '1' and r_ready = '1' else '0';
+
+    o_data <= r_mem(r_rd_head);
+    o_valid <= r_valid;
+    o_ready <= r_ready;
 
     p_fifo : process (i_clk) is
     begin
         if rising_edge(i_clk) then
-            if i_write = '1' then
-                r_mem(r_wr_head) <= i_data;
-                r_wr_head <= r_wr_head + 1;
+            if r_read = '1' then
+                r_rd_head <= (r_rd_head + 1) mod c_depth;
             end if;
 
-            if i_read = '1' then
-                r_out <= r_mem(r_rd_head);
-                r_rd_head <= r_rd_head + 1;
+            if r_write = '1' then
+                r_mem(r_wr_head) <= i_data;
+                r_wr_head <= (r_wr_head + 1) mod c_depth;
+            end if;
+
+            if r_read = '1' and r_write = '0' then
+                r_size <= r_size - 1;
+            elsif r_read = '0' and r_write = '1' then
+                r_size <= r_size + 1;
             end if;
         end if;
     end process p_fifo;
--- uart_tb.vhdl
+++ uart_tb.vhdl
@@ -7,6 +7,29 @@ end uart_tb;
 
 architecture tb of uart_tb is
 
+    component fifo
+        generic (
+            c_width : positive;
+            c_depth : positive;
+            c_thr : natural
+        );
+        port (
+            i_clk : in std_logic;
+            
+            i_valid : in std_logic;
+            o_ready : out std_logic;
+            i_data : in std_logic_vector(c_width - 1 downto 0);
+            
+            o_valid : out std_logic;
+            i_ready : in std_logic;
+            o_data : out std_logic_vector(c_width - 1 downto 0);
+
+            o_empty : out std_logic;
+            o_thr : out std_logic;
+            o_full : out std_logic
+        );
+    end component fifo;
+
     component uart_tx
         generic (
             c_clk_div : positive
@@ -37,9 +60,13 @@ architecture tb of uart_tb is
     signal r_tx_clk : std_logic := '0';
     signal r_txrx : std_logic;
     
-    signal r_tx_valid : std_logic := '1';
+    signal r_fifo_valid : std_logic := '1';
+    signal r_fifo_ready : std_logic;
+    signal r_fifo_data : std_logic_vector(7 downto 0) := (others => '0');
+
+    signal r_tx_valid : std_logic;
     signal r_tx_ready : std_logic;
-    signal r_tx_data : std_logic_vector(7 downto 0) := (others => '0');
+    signal r_tx_data : std_logic_vector(7 downto 0);
     
     signal r_rx_valid : std_logic;
     signal r_rx_ready : std_logic := '1';
@@ -49,6 +76,28 @@ begin
 
     r_clk <= not r_clk after 2.855445906432749 ns;
     r_tx_clk <= not r_tx_clk after 10 ns;
+
+    fifo_inst : fifo
+        generic map (
+            c_width => 8,
+            c_depth => 8,
+            c_thr => 4
+        )
+        port map (
+            i_clk => r_tx_clk,
+            
+            i_valid => r_fifo_valid,
+            o_ready => r_fifo_ready,
+            i_data => r_fifo_data,
+            
+            o_valid => r_tx_valid,
+            i_ready => r_tx_ready,
+            o_data => r_tx_data,
+            
+            o_empty => open,
+            o_thr => open,
+            o_full => open
+        );
 
     uart_tx_inst : uart_tx
         generic map (
@@ -77,8 +126,8 @@ begin
     p_tx : process (r_tx_clk)
     begin
         if rising_edge(r_tx_clk) then
-            if r_tx_valid = '1' and r_tx_ready = '1' then
-                r_tx_data <= std_logic_vector(unsigned(r_tx_data) + 1);
+            if r_fifo_valid = '1' and r_fifo_ready = '1' then
+                r_fifo_data <= std_logic_vector(unsigned(r_fifo_data) + 1);
             end if;
         end if;
     end process p_tx;
